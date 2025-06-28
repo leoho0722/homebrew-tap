c9f5edc7dcb7a20beddc02fdd9412434bb220718
leoho0722 <leo160918@gmail.com>
2025-06-28 16:00:25 +0800

    Fix architecture requirement for universal binary
    - Remove unnecessary if/else logic for architecture-specific URLs
    - Use single URL since we have a universal binary (x86_64 + arm64)
    - Fix depends_on arch syntax: [:x86_64, :arm64] instead of :x86_64_or_arm64
    - This resolves the 'unsatisfied requirement' build error

--- Formula/sf-symbols-generator.rb
+++ Formula/sf-symbols-generator.rb
@@ -3,16 +3,11 @@ class SfSymbolsGenerator < Formula
   homepage "https://github.com/leoho0722/SFSymbolsGenerator"
   version "1.3.0"
   
-  if Hardware::CPU.arm?
-    url "https://github.com/leoho0722/SFSymbolsGenerator/releases/download/1.3.0/sf-symbols-generator-1.3.0-macos.tar.gz"
-    sha256 "ebfb654a47c1494ef90818daa1bd4a0d97046ad2e5aa4a23676b96b252bc9cf3"
-  else
-    url "https://github.com/leoho0722/SFSymbolsGenerator/releases/download/1.3.0/sf-symbols-generator-1.3.0-macos.tar.gz"
-    sha256 "ebfb654a47c1494ef90818daa1bd4a0d97046ad2e5aa4a23676b96b252bc9cf3"
-  end
+  url "https://github.com/leoho0722/SFSymbolsGenerator/releases/download/1.3.0/sf-symbols-generator-1.3.0-macos.tar.gz"
+  sha256 "ebfb654a47c1494ef90818daa1bd4a0d97046ad2e5aa4a23676b96b252bc9cf3"
 
   depends_on :macos
-  depends_on arch: :x86_64_or_arm64
+  depends_on arch: [:x86_64, :arm64]
 
   def install
     bin.install "sf-symbols-generator"
